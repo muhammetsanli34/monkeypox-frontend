6eb27ccfc403b01c333e8fe37594a4d5df577085
Muhammet Emin Şanlı <muhammetsanli34@gmail.com>
2024-08-20 19:58:49 +0300

    Add geojson and ol dependencies, update index.css, and refactor index.tsx and App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Map } from "leaflet";
+import AppMap from "./components/AppMap";
+import { useEffect } from "react";
 
 function App() {
+  const position = { lat: 51.505, lng: -0.09 };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppMap center={position} zoom={13} />
     </div>
   );
 }
