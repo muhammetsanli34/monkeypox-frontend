32fe6aecc17b8b530ea774551b6ee8597b076a65
Muhammet Emin Şanlı <muhammetsanli34@gmail.com>
2024-08-22 22:13:06 +0300

    Add new font files and update CSS styles

--- src/components/AppMap.tsx
+++ src/components/AppMap.tsx
@@ -1,6 +1,16 @@
 import L from "leaflet";
-import countries from "../countries";
-import { useEffect, useRef } from "react";
+import countries from "../countries.json";
+import { useEffect, useMemo, useRef } from "react";
+import useCases from "../hooks/useCases";
+
+interface CaseData {
+  country: string;
+  iso3: string;
+  who_region: string;
+  month_lab: string;
+  cases: number;
+  deaths: number;
+}
 
 interface MapProps {
   center: { lat: number; lng: number };
@@ -8,43 +18,65 @@ interface MapProps {
 }
 
 const AppMap: React.FC<MapProps> = ({ center, style }) => {
+  const { calculatedCases, deathsByCountry, cases } = useCases();
   const mapRef = useRef<L.Map | null>(null);
 
   useEffect(() => {
-    if (mapRef.current) return;
+    if (!cases || mapRef.current) {
+      return;
+    }
     const map = L.map("map", {
       zoomControl: false,
-    }).setView([center.lat, center.lng], 11);
+    }).setView([center.lat, center.lng], 2);
 
     mapRef.current = map;
-
     L.tileLayer(
       `http://tile.stamen.com/terrain-background/{z}/{x}/{y}.jpg?key=${process.env.REACT_APP_MAPTILER_API_KEY}`,
       {
-        attribution: "© OpenStreetMap contributors",
+        // attribution: "© OpenStreetMap contributors",
         crossOrigin: true,
       }
     ).addTo(map);
 
-    countries.features.forEach((country) => {
-      L.geoJSON(country, {
-        style: {
-          color: "black",
-          weight: 0.5,
-          fillColor: "red",
-          fillOpacity: 0.5,
-        },
-      }).addTo(map);
-      if (country.lat && country.lng) {
-        L.circle([country.lat, country.lng], {
-          color: "red",
-          fillColor: "red",
-          fillOpacity: 0.5,
-          radius: 220000,
-        }).addTo(map);
+    const getColor = (cases: number) => {
+      if (cases >= 0 && cases <= 10) {
+        return "#FFCCCC";
+      } else if (cases > 10 && cases <= 100) {
+        return "#FF6666";
+      } else if (cases > 100 && cases <= 1000) {
+        return "#FF3333";
+      } else if (cases > 1000) {
+        return "#990000";
+      }
+      return "#FFCCCC";
+    };
+
+    Object.keys(calculatedCases).forEach((iso3) => {
+      const country = countries.features.find((country) => country.id === iso3);
+      if (country) {
+        L.geoJSON(country, {
+          style: {
+            color: "black",
+            weight: 0.5,
+            fillColor: getColor(calculatedCases[iso3]),
+            fillOpacity: 0.5,
+          },
+        })
+          .bindPopup(
+            `<b>${country.properties.name}</b><br>Cases: ${calculatedCases[iso3]}<br>Deaths: ${deathsByCountry[iso3]}`
+          )
+          .addTo(map);
+        if (country.lat && country.lng) {
+          L.circle([country.lat, country.lng], {
+            color: "red",
+            fillColor: "red",
+            fillOpacity: 0.5,
+            radius: Math.sqrt(calculatedCases[iso3]) * 5000,
+          }).addTo(map);
+        }
       }
     });
-  }, [center]);
+  }, [cases]);
 
   return (
     <div id="map" style={{ height: "400px", width: "100%", ...style }}></div>
---
+++ src/hooks/useCases.tsx
@@ -0,0 +1,71 @@
+import { useEffect, useMemo, useState } from "react";
+
+interface CaseData {
+  country: string;
+  iso3: string;
+  who_region: string;
+  month_lab: string;
+  cases: number;
+  deaths: number;
+}
+
+export default function useCases() {
+  const [cases, setCases] = useState<Array<CaseData> | null>(null);
+
+  const fetchCases = async () => {
+    fetch("http://trackmonkeypox.com:8000/get_all_data", {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setCases(data.monkey_pox_cases);
+      })
+      .catch((error) => console.error("Error:", error));
+  };
+
+  const calculatedCases = useMemo(() => {
+    const casesByCountry = cases?.reduce((acc, curr) => {
+      if (!acc[curr.iso3]) {
+        acc[curr.iso3] = 0;
+      }
+      acc[curr.iso3] += curr.cases;
+      return acc;
+    }, {} as Record<string, number>);
+    return casesByCountry || {};
+  }, [cases]);
+
+  const deathsByCountry = useMemo(() => {
+    const deathsByCountry = cases?.reduce((acc, curr) => {
+      if (!acc[curr.iso3]) {
+        acc[curr.iso3] = 0;
+      }
+      acc[curr.iso3] += curr.deaths;
+      return acc;
+    }, {} as Record<string, number>);
+    return deathsByCountry || {};
+  }, [cases]);
+
+  const totalCases = useMemo(() => {
+    return cases?.reduce((acc, curr) => acc + curr.cases, 0) ;
+  }, [cases]);
+
+  const totalDeaths = useMemo(() => {
+    return cases?.reduce((acc, curr) => acc + curr.deaths, 0) || 0;
+  }, [cases]);
+
+  useEffect(() => {
+    fetchCases();
+  }, []);
+
+  return {
+    cases,
+    calculatedCases,
+    deathsByCountry,
+    fetchCases,
+    totalCases,
+    totalDeaths,
+  };
+}
