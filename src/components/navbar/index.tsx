6405939d36f45d0a36c36399d4b3b3d903c676f9
Muhammet Emin Şanlı <muhammetsanli34@gmail.com>
2024-08-23 23:07:37 +0300

    feat: mobile menu

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -3,39 +3,47 @@ import MobileMenu from "../mobile-menu";
 
 export default function Navbar() {
   return (
-    <div className="container">
-      <div className="flex justify-around items-center py-4 text-sm">
+    <div className="container mx-auto">
+      <div className="flex justify-between items-center py-4">
         <Link to="/" className="flex items-center gap-2">
           <img src="logo.png" alt="logo" width={50} />
           <h1 className="text-lg font-bold">
-            TRACK
-            {/* <br /> */}
-            MONKEY
+            TRACK MONKEY
             <span className="text-bold" style={{ color: "#ca3b3b" }}>
               POX
             </span>
           </h1>
         </Link>
-
-        <div className="flex space-x-4">
-          <Link
-            to="/"
-            className="no-underline text-black
+        <div className="hidden justify-around items-center text-sm lg:flex">
+          <div className="flex space-x-4">
+            <Link
+              to="/"
+              className="no-underline text-black
             hover:text-primary hover:border-2 hover:border-main cursor-pointer  hover:rounded-md p-2
             animation duration-300 ease-in-out text-lg
             "
-          >
-            CANLI VERİ
-          </Link>
-          <Link
-            to="/symptoms"
-            className="no-underline text-black
+            >
+              CANLI VERİ
+            </Link>
+            <Link
+              to="/belirtiler"
+              className="no-underline text-black
             hover:text-primary hover:border-2 hover:border-main cursor-pointer  hover:rounded-md p-2
             animation duration-300 ease-in-out text-lg
             "
-          >
-            BELİRTİLER
-          </Link>
+            >
+              BELİRTİLER
+            </Link>
+            <Link
+              to="/maymun-cicegi"
+              className="no-underline text-black
+            hover:text-primary hover:border-2 hover:border-main cursor-pointer  hover:rounded-md p-2
+            animation duration-300 ease-in-out text-lg
+            "
+            >
+              MAYMUN ÇİÇEĞİ
+            </Link>
+          </div>
         </div>
         <MobileMenu />
       </div>
---
+++ src/constants/menuItems.ts
@@ -0,0 +1,14 @@
+export default [
+  {
+    text: "CANLI VERİ",
+    link: "/",
+  },
+  {
+    text: "BELİRTİLER",
+    link: "/symptoms",
+  },
+  {
+    text: "MAYMUN ÇİÇEĞİ",
+    link: "/maymun-cicegi",
+  }
+];
